b30aea98f499272e7697172476e1b4094fa1e221
Kalki767 <kalkidankidane3@gmail.com>
2025-03-26 13:55:50 +0300

    Update mock chat repository to implement get chat by participants method

--- test/test_repository/mocks/mock_chat_repository.go
+++ test/test_repository/mocks/mock_chat_repository.go
@@ -11,8 +11,8 @@ type MockChatRepository struct {
 	mock.Mock
 }
 
-func (m *MockChatRepository) CreateChat(ctx context.Context, chat *domain.Chat) (primitive.ObjectID, error) {
-	args := m.Called(ctx, chat)
+func (m *MockChatRepository) CreateChat(ctx context.Context, SenderID primitive.ObjectID, ReceiverID primitive.ObjectID) (primitive.ObjectID, error) {
+	args := m.Called(ctx, SenderID, ReceiverID)
 	return args.Get(0).(primitive.ObjectID), args.Error(1)
 }
 
@@ -40,4 +40,12 @@ func (m *MockChatRepository) UpdateChat(ctx context.Context, chatID primitive.Ob
 func (m *MockChatRepository) DeleteChat(ctx context.Context, chatID primitive.ObjectID) error {
 	args := m.Called(ctx, chatID)
 	return args.Error(0)
+}
+
+func (m *MockChatRepository) GetChatByParticipants(ctx context.Context, SenderID primitive.ObjectID, ReceiverID primitive.ObjectID) (*domain.Chat, error) {
+	args := m.Called(ctx, SenderID, ReceiverID)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*domain.Chat), args.Error(1)
 }
